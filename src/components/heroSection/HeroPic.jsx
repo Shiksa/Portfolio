e38815aa0ace926fbf455d79fd69a0e4c8c72517
Sayantan Pakhira <shiksayantanpakhira74@gmail.com>
2025-11-13 12:03:36 +0530

    responsive carosoul issue fixed

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -7,7 +7,8 @@ const HeroPic = () => {
       <img src="/sayantan 1.png" alt="Sayantan" className='max-h-[450px] w-auto' />
 
       <div className='absolute -z-10 flex justify-center items-center animate-pulse scale-80'>
-        <RxCircle className='md:h-[90%] sm:h-[120%] min-h-[470px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]' /></div>
+        {/* <RxCircle className='md:h-[90%] sm:h-[120%] min-h-[470px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]' /> */}
+      </div>
     </div>
   )
 }
