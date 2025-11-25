d7f1db06a24ab802457ac29f92b58ff5292ec2ba
Sayantan Pakhira <shiksayantanpakhira74@gmail.com>
2025-11-25 19:19:39 +0530

    Icon added for website

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -5,7 +5,7 @@ const NavbarLogo = () => {
     <div className='h-[36px]  overflow-hidden flex'>
       <h1 className='text-white text-2xl sm:hidden md:block justify-center items-center'>Sayantan Pakhira</h1>
       {/* <h1 className='text-white font-special font-extrabold text-4xl sm:block md:hidden'>SP</h1> */}
-      <img src="/sp logo.png" alt="SP" className='sm:block md:hidden object-contain' />
+      <img src="/spLogo.png" alt="SP" className='sm:block md:hidden object-contain' />
     </div>
   )
 }
