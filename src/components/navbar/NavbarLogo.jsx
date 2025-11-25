b4e71ebabcf2d9a014bae7973cb7d5ecd2f00533
Sayantan Pakhira <shiksayantanpakhira74@gmail.com>
2025-11-25 19:10:42 +0530

    Logo uploaded

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const NavbarLogo = () => {
   return (
-    <div>
-      <h1 className='text-white text-2xl sm:hidden md:block'>Sayantan Pakhira</h1>
-      <h1 className='text-white font-special font-extrabold text-4xl sm:block md:hidden'>SP</h1>
+    <div className='h-[36px]  overflow-hidden flex'>
+      <h1 className='text-white text-2xl sm:hidden md:block justify-center items-center'>Sayantan Pakhira</h1>
+      {/* <h1 className='text-white font-special font-extrabold text-4xl sm:block md:hidden'>SP</h1> */}
+      <img src="/sp logo.png" alt="SP" className='sm:block md:hidden object-contain' />
     </div>
   )
 }
